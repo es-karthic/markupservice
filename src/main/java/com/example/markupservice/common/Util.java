63d64370ce72967005a40b3ff4cbcc8bb2b9e4d9
eskarthic <eskarthic@gmail.com>
2016-12-10 11:03:53 +0530

    adding gitignore file

---
+++ src/main/java/com/example/markupservice/common/Util.java
@@ -0,0 +1,7 @@
+package com.example.markupservice.common;
+
+/**
+ * Created by work on 12/10/16.
+ */
+public class Util {
+}
---
+++ src/main/java/com/example/markupservice/exception/InvalidNumberException.java
@@ -0,0 +1,7 @@
+package com.example.markupservice.exception;
+
+/**
+ * Created by work on 12/10/16.
+ */
+public class InvalidNumberException {
+}
